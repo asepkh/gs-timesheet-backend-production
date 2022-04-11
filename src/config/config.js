ad85604e75a3c000976322da17d9edce9e51253b
Asep Khairul Anam <asep.khair@gmail.com>
2022-04-11 12:48:45 +0700

    change config 2

--- src/config/config.js
+++ src/config/config.js
@@ -16,12 +16,12 @@ const x = {
     dialect: process.env.DB_DIALECT,
   },
   production: {
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASS,
+    // database: process.env.DB_NAME,
+    // host: process.env.DB_HOST,
+    // dialect: process.env.DB_DIALECT,
     use_env_variable: "DATABASE_URL",
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
   },
 };
 module.exports = x;
