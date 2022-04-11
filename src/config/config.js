3a6dca46a2a37ac5e627216a498a5fc3d110f967
Asep Khairul Anam <asep.khair@gmail.com>
2022-04-11 13:18:16 +0700

    try fix prod

--- src/config/config.js
+++ src/config/config.js
@@ -24,6 +24,13 @@ const x = {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     protocol: "postgres",
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
 module.exports = x;
