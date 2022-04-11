b9b0f33f46eaf6d2129db9426db4c46268fbcb47
Asep Khairul Anam <asep.khair@gmail.com>
2022-04-11 13:54:44 +0700

    add production config

--- src/config/config.js
+++ src/config/config.js
@@ -2,11 +2,16 @@ require("dotenv").config();
 
 const x = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    protocol: "postgres",
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     username: process.env.DB_USER,
