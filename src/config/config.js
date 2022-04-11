a8bd3cff98b06d989cc3b7c498ca91efb7bfb2b8
Asep Khairul Anam <asep.khair@gmail.com>
2022-04-11 14:13:21 +0700

    change config 4

--- src/config/config.js
+++ src/config/config.js
@@ -14,11 +14,16 @@ const x = {
     },
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    protocol: "postgres",
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     // username: process.env.DB_USER,
